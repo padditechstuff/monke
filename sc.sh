05fe3636055d9384a1fb1456c46088f2c2ea0021
padditechstuff <126967920+padditechstuff@users.noreply.github.com>
2023-06-27 20:40:07 +0700

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -2,4 +2,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.19.3/xmrig-6.19.3-linux
 tar xf xmrig-6.19.3-linux-x64.tar.gz
 cd xmrig-6.19.3
 chmod +x *
-./xmrig --url randomxmonero.eu-west.nicehash.com:3380 --user NHbaa7ccJtrWoimE36Q4maFjz3iFafwQNK2N --pass x -a rx/0
+./xmrig --tls --max-cpu-usage 100 --url randomxmonero.auto.nicehash.com:3380 --user NHbaa7ccJtrWoimE36Q4maFjz3iFafwQNK2N --pass x -a rx/0 -k nicehash --coin monero
