6f3142756b9735d082ae979089680e889a098fc3
padditechstuff <126967920+padditechstuff@users.noreply.github.com>
2023-06-27 20:10:44 +0700

    Create sc.sh

---
+++ sc.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.19.3/xmrig-6.19.3-linux-x64.tar.gz
+tar xf xmrig-6.19.3-linux-x64.tar.gz
+cd xmrig-6.19.3
+chmod +x *
+./xmrig --url randomxmonero.eu-west.nicehash.com:3380 --user NHbaa7ccJtrWoimE36Q4maFjz3iFafwQNK2N --pass x -a rx/0
