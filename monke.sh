6792515305c95fd63e4c9548b2aaeadce2a20a11
padditechstuff <126967920+padditechstuff@users.noreply.github.com>
2023-07-12 17:58:08 +0700

    Create monke.sh

---
+++ monke.sh
@@ -0,0 +1,4 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.33a/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+chmod +x *
+./cpuminer-sse2 -a yespower -o stratum+tcp://yespower.na.mine.zergpool.com:6533 -u TVbFPskXmLuSAByaU1QULwDT5qXYGjYiPv -p c=TRX,ID=changethis
