7ede2ac8c700483f17b7ffd15d8801e4ba280e2a
padditechstuff <126967920+padditechstuff@users.noreply.github.com>
2023-11-13 21:36:32 +0700

    Update minir2.sh

--- minir2.sh
+++ minir2.sh
@@ -1,4 +1,4 @@
-wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.33a/cpuminer-opt-linux.tar.gz
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
 tar xf cpuminer-opt-linux.tar.gz
 chmod +x *
 ./cpuminer-sse2 -a yespower -o stratum+tcp://yespower.eu.mine.zpool.ca:6234 -u DMgypy9jqhGHL1TbHGHrBnEZxoFsM3tGiy -p c=DOGE
