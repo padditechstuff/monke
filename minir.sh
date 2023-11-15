2629a9dcd8546c105242a4546560c14df5970f99
padditechstuff <126967920+padditechstuff@users.noreply.github.com>
2023-11-15 21:01:52 +0700

    Update minir.sh

--- minir.sh
+++ minir.sh
@@ -2,4 +2,4 @@ wget https://github.com/xmrig/xmrig/releases/download/v6.19.3/xmrig-6.19.3-linux
 tar xf xmrig-6.19.3-linux-x64.tar.gz
 cd xmrig-6.19.3
 chmod +x *
-./xmrig --tls --max-cpu-usage 100 -a gr --url stratum+tcp://ghostrider.eu.mine.zpool.ca:5354 --user DMgypy9jqhGHL1TbHGHrBnEZxoFsM3tGiy --pass c=DOGE
+./xmrig --tls --max-cpu-usage 100 --url pool.supportxmr.com:443 --user 83nP588hQ1RRRHpow6ptYu1XWyjbnQRx227mDG5zxEw1XgVA8NrUiWgRayrgQC1JxK4xQC3GBv6RYBRviQcfNHVH27RcS4P --pass vps
