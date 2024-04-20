d935552c06da09ff6105d729fbcecfa3c95454c1
padditechstuff <126967920+padditechstuff@users.noreply.github.com>
2024-04-20 18:33:01 +0700

    Create rvn.sh

---
+++ rvn.sh
@@ -0,0 +1,4 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+chmod +x *
+./cpuminer-avx -a minotaurx -o stratum+tcp://minotaurx.na.mine.zpool.ca:7019 -u RRybdGViabAGQNQrY3sDazAjqN839bz3zm -p c=RVN
