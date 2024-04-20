8e91fc6ba6898e4a89a8fe1f2f6e0e3e27aa4052
padditechstuff <126967920+padditechstuff@users.noreply.github.com>
2024-04-20 22:41:17 +0700

    Create rvn2.sh

---
+++ rvn2.sh
@@ -0,0 +1,4 @@
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.36/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+chmod +x *
+./cpuminer-avx -a yespower -o stratum+tcp://yespower.na.mine.zpool.ca:6234 -u RRybdGViabAGQNQrY3sDazAjqN839bz3zm -p c=RVN
